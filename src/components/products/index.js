f688d5e2b60be01a7060ebf1810e09decba44876
Alex <alexandr.webwork@gmail.com>
2022-09-11 09:05:23 +0300

    env

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,8 +1,8 @@
 function Products() {
   return (
     <>
-      <div class="flex flex-wrap -mx-px overflow-hidden">
-        <div class="my-px px-px w-1/3 overflow-hidden sm:w-1/2">1</div>
+      <div className="flex flex-wrap -mx-px overflow-hidden">
+        <div className="my-px px-px w-1/3 overflow-hidden sm:w-1/2">1</div>
       </div>
     </>
   );
