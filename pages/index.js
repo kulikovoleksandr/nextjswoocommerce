327e40461b18870aaaad3ad1dc4555602f67e9cd
Alex <alexandr.webwork@gmail.com>
2022-09-11 08:55:43 +0300

    products

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@ import Header from "../src/components/layouts/header";
 import Footer from "../src/components/layouts/footer";
 import axios from "axios";
 import { HEADER_FOOTER_ENDPOINT } from "../src/utils/constants/endpoints";
+import Products from "../src/components/products";
 
 export default function Home({ data }) {
   const { header, footer } = data;
   return (
     <div>
       <Header header={header} />
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <div className="container mx-auto px-10">
+        <Products />
+      </div>
       <Footer footer={footer} />
     </div>
   );
@@ -18,6 +19,9 @@ export default function Home({ data }) {
 
 export async function getStaticProps() {
   const { data } = await axios.get(HEADER_FOOTER_ENDPOINT);
+  const { data: products } = await axios.get(
+    "http://localhost:3000/api/get-products"
+  );
   return {
     props: data || {},
     revalidate: 1,
---
+++ src/components/products/index.js
@@ -0,0 +1,11 @@
+function Products() {
+  return (
+    <>
+      <div class="flex flex-wrap -mx-px overflow-hidden">
+        <div class="my-px px-px w-1/3 overflow-hidden sm:w-1/2">1</div>
+      </div>
+    </>
+  );
+}
+
+export default Products;
